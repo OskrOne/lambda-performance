ad4d97b8e202b5dce1e216b7cad04a49e5c13e2d
Oscar López <ollopezc@gbm.com.mx>
2019-05-24 14:10:06 -0500

    C# example was added

---
+++ csharp/Dynamo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+
+namespace AwsDotnetCsharp
+{
+    class Dynamo
+    {
+        private readonly AmazonDynamoDBClient amazonDynamoDbClient;
+        private readonly string AssetTableName = "Assets";
+
+        public Dynamo() {
+            amazonDynamoDbClient = new AmazonDynamoDBClient();
+        }
+        /// <summary>
+        /// Get Item using Document Level
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Document> getItem(string contractId) {
+            var table = Table.LoadTable(amazonDynamoDbClient, AssetTableName);
+            var result = await table.GetItemAsync(contractId);
+
+            if (result == null) {
+                throw new Exception("There are no assets with id " + contractId);
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// Insert 10,000 items in DynamoDB
+        /// </summary>
+        private async void insertItemsMassivelyAsync()
+        {
+            Random random = new Random();
+            var table = Table.LoadTable(amazonDynamoDbClient, AssetTableName);
+            
+            for (int index = 3; index < 10000; index++) {
+                var asset = new Document();
+                asset["ContractId"] = "A" + index;
+                asset["InstrumentId"] = random.Next(100000, 999999);
+                var result = await table.PutItemAsync(asset);
+            }
+        }
+
+        /// <summary>
+        /// Get Item using Dynamo Service Level API
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private async Task<Dictionary<string, AttributeValue>> getItemUsingServices(string contractId)
+        {
+            Dictionary<string, AttributeValue> key = new Dictionary<string, AttributeValue> {
+                {
+                    "ContractId", new AttributeValue{ S = contractId }
+                }
+            };
+
+            GetItemRequest getItemRequest = new GetItemRequest
+            {
+                TableName = AssetTableName,
+                Key = key
+            };
+
+            var result = await amazonDynamoDbClient.GetItemAsync(getItemRequest);
+            if (result == null)
+            {
+                throw new Exception("There are no assets with id " + contractId);
+            }
+
+            return result.Item;
+        }
+    }
+}
