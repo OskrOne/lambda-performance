d75b6aa5ca83d28a88f09c03d9b672b7725aaf92
Oscar López <ollopezc@gbm.com.mx>
2019-06-03 13:32:07 -0500

    Python example was added

--- python3/handler.py
+++ python3/handler.py
@@ -1,24 +1,34 @@
+from __future__ import print_function
 import json
+import boto3
+import decimal
 
+# This is because for some reason, json dumps does not support decimal 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Assets')
+    body = json.loads(event['body'])
+
+    res = table.get_item(
+        Key={
+            'ContractId': body['ContractId']
+        }
+    )
+
+    item = res['Item']
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(item, cls=DecimalEncoder)
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
